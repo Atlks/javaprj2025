5197b388a5d89817290588a2fd8473c0a82dfc74
ati <ati@uke.com>
2024-09-17 11:54:44 +0800

    sprbt

---
+++ AppSprtStarter.java
@@ -0,0 +1,14 @@
+
+
+public class AppSprtStarter {
+
+    public static void main(String[] args){
+  libx.Application.main(new String[]{ });
+      //  libx.Application().main(null);
+    }
+
+
+
+
+
+}
