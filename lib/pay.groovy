830000453f030b9912ecf6fe1980cef22f4f2db5
ati <ati@uke.com>
2024-09-18 16:38:10 +0800

    grv...

--- lib/pay.groovy
+++ lib/pay.groovy
@@ -3,6 +3,11 @@ import groovy.lang.GroovyShell
 def print888() {
     println 888999
 }
+def Object evalFil(String funFil) {
+    def functions = new GroovyShell().evaluate(new File(funFil))
+    functions
+}
+
 
 // 调用函数
 print888()
@@ -11,16 +16,18 @@ println 222
 
 // main.groovy
 def funFil = 'BscEncGrv.groovy'
-def functions = new GroovyShell().evaluate(new File(funFil))
+Object fns = evalFil(funFil)
+
+
 
 
 // 将函数直接引入 当前作用域  cant effect
-//this.&encodeJsonGrv =functions .&encodeJsonGrv
+//this.&encodeJsonGrv =fns .&encodeJsonGrv
 
 
 
 // 现在可以直接使用 encodeJsonGrv
-def resultJson = functions.encodeJsonGrv([key: 553])
+def resultJson = fns.encodeJsonGrv([key: 553])
 
 println resultJson
 //lib.BscEncGrv.encodeJsonGrv(333)
