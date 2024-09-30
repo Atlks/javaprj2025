ad5b66e0d066d1e777d551257f11ef8a5b981c13
ati <ati@uke.com>
2024-09-30 15:38:21 +0800

    kvdb rocket db

---
+++ lib/UtilKvdbLevDb.java
@@ -0,0 +1,70 @@
+package lib;
+
+import java.util.Map;
+import org.fusesource.leveldbjni.JniDBFactory;
+import org.rocksdb.*;
+import java.io.File;
+public class UtilKvdbLevDb {
+
+
+    public static void main(String[] args) throws  Exception {
+
+
+        // Example Map to insert
+        Map<String, Object> exampleMap = Map.of(
+                "id", "John Doe",
+                "age", 30,
+                "city", "New York"
+        );
+
+        // Example dynamic database URL
+        String dbUrl = "aaaa.sqlt.db";
+
+        AddRow(exampleMap, dbUrl);
+
+
+        System.out.println("Map inserted successfully!");
+
+    }
+
+    private static void AddRow(Map<String, Object> exampleMap, String dbUrl) throws RocksDBException {
+
+        // 数据库路径
+        String dbPath = "rockt.db";
+
+
+            // 创建 RocksDB 数据库
+            Options options = new Options().setCreateIfMissing(true);
+        RocksDB db = null;
+        try {
+            db = RocksDB.open(options, dbPath);
+        } catch (RocksDBException e) {
+            throw new RuntimeException(e);
+        }
+
+        // 写入数据
+            db.put("key1".getBytes(), "value1".getBytes());
+            db.put("key2".getBytes(), "value2".getBytes());
+
+
+    }
+
+//    private static void AddRow(Map<String, Object> exampleMap, String dbUrl) {
+//
+//
+//        import org.fusesource.leveldbjni.DB;
+//        // 数据库路径
+//        File dbFile = new File("path/to/your/db");
+//
+//        // 打开数据库
+//        try (DB db = JniDBFactory.open(dbFile)) {
+//            // 写入数据
+//            db.put("key1".getBytes(), "value1".getBytes());
+//            db.put("key2".getBytes(), "value2".getBytes());
+//        }
+//
+//
+//    }
+
+
+}
